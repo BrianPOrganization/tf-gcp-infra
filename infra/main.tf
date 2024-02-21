e79033fad262d04ec4b7b073a3184f05149f9938
Brian Marcel Patrao <patrao.b@northeastern.edu>
2024-02-21 16:18:58 -0500

    assignment4

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "vpc_list" {
   name                            = var.vpc_list[count.index]
   auto_create_subnetworks         = false
   delete_default_routes_on_create = true
-  routing_mode                    = "REGIONAL"
+  routing_mode                    = var.routing_mode
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
@@ -28,5 +28,44 @@ resource "google_compute_route" "webapp_route" {
   next_hop_gateway = "default-internet-gateway"
 }
 
+resource "google_compute_firewall" "vpc_firewall" {
+  name    = "webapp-firewall-${count.index}"
+  count   = length(var.vpc_list)
+  network = google_compute_network.vpc_list[count.index].name
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
 
+resource "google_compute_firewall" "no_ssh" {
+  name    = "no-ssh-${count.index}"
+  count   = length(var.vpc_list)
+  network = google_compute_network.vpc_list[count.index].name
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "webapp_instance" {
+  machine_type = var.machine_type
+  count        = length(var.vpc_list)
+  name         = "webapp-instance-${count.index}"
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = var.image
+      type  = var.image_type
+      size  = var.image_size
+    }
+  }
+  network_interface {
+    network    = google_compute_network.vpc_list[count.index].name
+    subnetwork = google_compute_subnetwork.webapp_subnet[count.index].name
+    access_config {}
+  }
+}
 
--- infra/variables.tf
+++ infra/variables.tf
@@ -15,3 +15,51 @@ variable "db_cidr_range" {
   type        = string
   default     = "10.168.1.0/24"
 }
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-east1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-east1-c"
+}
+
+variable "project" {
+  description = "GCP project"
+  type        = string
+  default     = "brian-csye6225"
+}
+
+variable "routing_mode" {
+  description = "Routing mode"
+  type        = string
+  default     = "REGIONAL"
+}
+
+variable "machine_type" {
+  description = "Machine type"
+  type        = string
+  default     = "n1-standard-1"
+}
+
+variable "image" {
+  description = "Custom Image"
+  type        = string
+  default     = "packer-1708537706"
+}
+
+variable "image_size" {
+  description = "Image size"
+  type        = number
+  default     = 100
+}
+
+variable "image_type" {
+  description = "Image type"
+  type        = string
+  default     = "pd-balanced"
+}
