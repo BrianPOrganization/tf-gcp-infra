eab37b46760ef2da7396127aca0aa3051d71af5e
Brian Marcel Patrao <patrao.b@northeastern.edu>
2024-02-21 16:18:58 -0500

    assignment4

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   # project id
-  project = "brian-csye6225"
-  region  = "us-east1"
-  zone    = "us-east1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
