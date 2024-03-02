5bb2693c3bfb9a8c44bcc45dc7de069895a022ad
Brian Marcel Patrao <patrao.b@northeastern.edu>
2024-03-02 14:21:56 -0500

    Assignment 5

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,20 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~>4"
+    }
+  }
+}
+
 provider "google" {
   # project id
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+provider "google-beta" {
   project = var.project
   region  = var.region
   zone    = var.zone
